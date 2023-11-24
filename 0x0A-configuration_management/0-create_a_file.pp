fef20b9218e7191c3fd1a08e8c54fc446bed0311
hichamaitlahsen5 <hichamaitlahsen5@gmail.com>
2023-11-24 06:48:46 -0800

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 #The code will create a file name school inside the tmp Directory.
 
 file { '/tmp/school':
-ensure	=> file,
-content	=> 'I love Puppet',
-mode	=> '0744',
-owner	=> 'www-data',
-group	=> 'www-data',
+	ensure	=> file,
+	mode	=> '0744',
+	owner	=> 'www-data',
+	group	=> 'www-data',
+	content	=> 'I love Puppet',
 }
